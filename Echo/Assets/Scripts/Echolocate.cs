997de66e8703aafce0d0f8628c930862dbd6b6f6
Adam Cantor <adamcantor22@gmail.com>
2021-05-02 18:11:52 -0400

    Improved echolocation

--- Echo/Assets/Scripts/Echolocate.cs
+++ Echo/Assets/Scripts/Echolocate.cs
@@ -7,15 +7,37 @@ public class Echolocate : MonoBehaviour
 
     public GameObject trail;
     public int trails = 6;
+    public AudioClip[] clips;
+
+    AudioSource speaker;
+    public float cooldown;
+    float cooldownTimer;
+
+    void Start() {
+        speaker = this.GetComponent<AudioSource>();
+        cooldownTimer = cooldown;
+    }
   
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0)) {
+        
+        if(cooldownTimer > 0f)
+            cooldownTimer -= Time.deltaTime;
+
+
+        if (cooldownTimer <= 0f && Input.GetKeyDown(KeyCode.Mouse0)) {
             for (int i = 0; i < trails; i++)
             {
-                GameObject instance = (GameObject)Instantiate(trail, transform.position, transform.rotation);
-                instance.GetComponent<ParticleMover>().SetDirection(new Vector3(0f, -.1f, 1f));
+                GameObject instance = (GameObject)Instantiate(trail, transform.position, Quaternion.Euler(0f,0f,0f));
+                instance.GetComponent<ParticleMover>().SetDirection(transform.forward);
+                Debug.Log(transform.forward);
+                cooldownTimer = cooldown;
+                PlaySound(0);
             }
         }
+    }
+
+    public void PlaySound(int sound) {
+        speaker.PlayOneShot(clips[sound], 0.5f);
     }
 }
--- Echo/Assets/Scripts/ParticleMover.cs
+++ Echo/Assets/Scripts/ParticleMover.cs
@@ -5,23 +5,35 @@ using UnityEngine;
 public class ParticleMover : MonoBehaviour
 {
 
-    Vector3 startPos;
+    Vector3 cameraPos;
     Vector3 direction;
-    float speed = 4f;
+    float speed = 5f;
     bool backwards = false;
+
+    Echolocate echo;
 
     void Start()
     {
-        startPos = transform.position;
+        echo = Camera.main.GetComponent<Echolocate>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += direction * speed * Time.deltaTime;
-        if (Vector3.Distance(transform.position, startPos) < 1f && backwards) {
-            Destroy(gameObject);
+        if(backwards && cameraPos != Camera.main.transform.position) {
+            GoTowardsCamera();
         }
+            
+        transform.position += direction * speed * Time.deltaTime;
+        if (Vector3.Distance(transform.position, Camera.main.transform.position) < 0.5f && backwards) {
+            Destroy(gameObject);
+            echo.PlaySound(1);
+        }
+    }
+
+    void GoTowardsCamera() {
+        cameraPos = Camera.main.transform.position;
+        SetDirection(Vector3.Normalize(cameraPos - transform.position)) ;
     }
 
     public void SetDirection(Vector3 dir) {
@@ -29,8 +41,9 @@ public class ParticleMover : MonoBehaviour
     }
 
     void OnCollisionEnter(Collision other) {
-        Debug.Log("COLLISION");
-        direction *= -1;
-        backwards = true;
+        if(!backwards) {
+            backwards = true;
+            GoTowardsCamera();
+        }
     }
 }
