b6ec2795ca1f02e6c475f2c51b5cc4fbf53dd50d
Unknown <adamcantor22@gmail.com>
2021-05-10 16:43:04 -0400

    door and key mechanics

--- Echo/Assets/Scripts/KeyFinder.cs
+++ Echo/Assets/Scripts/KeyFinder.cs
@@ -6,20 +6,44 @@ public class KeyFinder : MonoBehaviour
 {
     
     GameObject key;
+    GameObject door;
     bool hasKey = false;
+
+    public GameObject keyText;
+    float textDuration = 5f;
+    float textTimer = 0f;
 
     void Start()
     {
         key = GameObject.FindGameObjectWithTag("Key");
+        door = GameObject.FindGameObjectWithTag("Door");
     }
 
     
     void Update()
     {
+        if(textTimer > 0f) {
+            textTimer -= Time.deltaTime;
+            if(textTimer <= 0f) {
+                keyText.SetActive(false);
+            }
+        }
+
         if(!hasKey && Vector3.Distance(key.transform.position, transform.position) <= 0.5f) {
             hasKey = true;
             GetComponent<Echolocate>().PlaySound(2);
             Destroy(key);
+        }
+
+        if(Vector3.Distance(door.transform.position, transform.position) <= 0.5f) {
+            if(!hasKey && !keyText.activeInHierarchy) {
+                keyText.SetActive(true);
+                textTimer = textDuration;
+            }
+
+            else if(hasKey) {
+                //go to end screen
+            }
         }
     }
 }
